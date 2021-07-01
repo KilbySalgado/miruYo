fb15c187f33dbec37eaf36c704d49663da53ef11
davidalbertocolindresfortin <52940871+davidalbertocolindresfortin@users.noreply.github.com>
2021-07-01 11:10:32 -0600

    Avance de Proyecto

---
+++ src/api-miruyo/index.js
@@ -0,0 +1,5 @@
+import getEnvVars from "../../environment";
+
+const { apiUrl } = getEnvVars();
+
+//const miruyo = "https://api.jikan.moe/v3";
---
+++ src/compomentes/Inicio.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Inicio = () => {
+  return (
+    <View>
+      <Text>miruYo Aplicacion de Anime JikanAPI</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default Inicio;
