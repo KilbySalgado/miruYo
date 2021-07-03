39659b044bd08e9ae89d51a38f4b489daa2594dc
davidalbertocolindresfortin <davidcolindres030@gmail.com>
2021-07-03 17:41:19 -0600

    ApiFuncional

--- src/api-miruyo/index.js
+++ src/api-miruyo/index.js
@@ -1,5 +1,10 @@
-import getEnvVars from "../../environment";
-
-const { apiUrl } = getEnvVars();
-
-//const miruyo = "https://api.jikan.moe/v3";
+export const AnimeApis = async (anime) => {
+  try {
+    const endpoint = `https://api.jikan.moe/v3/search/anime?q=${anime}`;
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    return console.log(error);
+  }
+};
