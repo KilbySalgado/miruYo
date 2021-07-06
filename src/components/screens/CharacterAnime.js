3a863ea07f622f2adf5fdc29a393be76dc250d2d
davidalbertocolindresfortin <52940871+davidalbertocolindresfortin@users.noreply.github.com>
2021-07-06 17:29:40 -0600

    Cambio de Carpeta de Compomentes

---
+++ src/components/screens/CharacterAnime.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { AnimeCharacter } from "../../api";
+import CardList from "../shared/CardList";
+import CardListCharacters from "../shared/CardListCharacters";
+
+const CharacterAnime = ({ route }) => {
+  const { idAnimeCharacter } = route.params;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
+  const getCharacter = async () => {
+    const data = await AnimeCharacter(idAnimeCharacter);
+    setCharacters(data);
+  };
+
+  return (
+    <View>
+      <CardListCharacters animeCharacters={characters} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CharacterAnime;
