14617a91c0b5a540f7c47be65c8577cfacfc5134
KilbySalgado <kir_salgadoc@unicah.edu>
2021-07-26 13:29:27 -0600

    se agrego un bottom navigation tab

--- src/components/navigation/TabNavigation.js
+++ src/components/navigation/TabNavigation.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
 import Inicio from '../screens/Inicio';
-import CharacterAnime from '../screens/CharacterAnime';
 import TopAnime from '../screens/TopAnime';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-const Tab = createMaterialTopTabNavigator();
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const TabNav= ()=> {
-return (
-        <Tab.Navigator tabBarOptions={{
-            labelStyle: {color: "#ffffff", fontSize: 12, textAlign: 'center', fontWeight: 'bold'},
-            style:{backgroundColor:"#ff5733",}}} tabBarPosition="top">
-            <Tab.Screen name = "Inicio" component={Inicio}/>
-            <Tab.Screen name = "Top" component={TopAnime}/>
+const Tab = createBottomTabNavigator();
+const TabNav = () => {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            tabBarOptions={{
+                activeTintColor: '#e91e63',
+            }}
+        >
+            <Tab.Screen
+                name="Home"
+                component={Inicio}
+                options={{
+                    tabBarLabel: 'Home',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="TopAnime"
+                component={TopAnime}
+                options={{
+                    tabBarLabel: 'TopAnime',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="bell" color={color} size={size} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
-);
+    );
 }
 
 
