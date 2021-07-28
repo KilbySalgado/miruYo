df971ccc5c3172009396c7e5c4d7ea57c7f335dd
KilbySalgado <kir_salgadoc@unicah.edu>
2021-07-27 23:45:50 -0600

    Creacion de la funcionalidad del Login con firebase

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import StackNavigate from './src/components/navigation/StackNavigation';
+import { Provider as AuthProvider } from './src/providers/AuthContext';
 
 export default function App() {
   return (
+    <AuthProvider>
       <StackNavigate />
+    </AuthProvider>
   );
 }
