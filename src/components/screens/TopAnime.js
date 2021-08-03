8570ffb442bb05311931ae8aa6aa3c6814001fee
KilbySalgado <kir_salgadoc@unicah.edu>
2021-08-03 17:39:55 -0600

    Arreglo de los Conflictos de los Merge

--- src/components/screens/TopAnime.js
+++ src/components/screens/TopAnime.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Text, Dimensions } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { AnimeTop } from "../../api";
 import CardList from "../shared/CardList";
 
-const {width, height} = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 
 const TopAnime = ({ navigation }) => {
   const [animeTop, setAnimeTop] = useState([]);
@@ -18,23 +19,25 @@ const TopAnime = ({ navigation }) => {
   };
 
   return (
-  <View style={styles.container}>
-      {animeTop.length ? (
-        <View>
-          <CardList animeData={animeTop} navigation={navigation} />
-        </View>
-      ) : (
-        <Text>No se ha cargado la api de manera correcta</Text>
-      )}
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        {animeTop.length ? (
+          <View>
+            <CardList animeData={animeTop} navigation={navigation} />
+          </View>
+        ) : (
+          <Text>No se ha cargado la api de manera correcta</Text>
+        )}
+      </View>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "#D8E3E7",
-      flex: 1,
-      width: width,
-      height: height,
-    },
+  container: {
+    backgroundColor: "#D8E3E7",
+    flex: 1,
+    width: width,
+    height: height,
+  },
 });
 export default TopAnime;
